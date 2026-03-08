bcb6c0c80816e5d13af438a03dffad28d40f28c5
xhiti <mexhitkurti10@gmail.com>
2026-03-08 19:45:35 +0100

    Update v2

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: 'next/core-web-vitals',
+    rules: {
+        'react/no-unescaped-entities': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-empty-object-type': 'off',
+        'prefer-const': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        '@next/next/no-img-element': 'off',
+        '@next/next/no-html-link-for-pages': 'off',
+    },
+}
